fd3f06d1b8f3aaceda2062e719bf468b5a790546
kamel <mohamed.kamel.m73@gmail.com>
2023-07-16 03:35:24 +0300

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-  validates :username,presence: true,length: {minimum:4,maximum:20}
+  validates :username,presence: true,
+                        length: {minimum:3,maximum:25},
+                        uniqueness: {case_sensetive:false}
+EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email,presence: true,
+                        length: {maximum:105},
+                        uniqueness: {case_sensetive:false},
+                        format:{with:EMAIL_REGEX}
 end
